066f50620928c37c53684fcdac6f1fa7bab62711
İlker Kılıç <ilkrklc@gmail.com>
2020-11-21 14:34:19 +0300

    Fix index unit test parameter bug

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -2,6 +2,6 @@ import exampleMethod from '../index';
 
 describe('index', () => {
   it('should multiply by 2', () => {
-    expect(exampleMethod(4)).toBe(8);
+    expect(exampleMethod({ n: 4 })).toBe(8);
   });
 });
