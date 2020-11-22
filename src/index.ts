cdfeb874004f5feb92161b129c8b5c0677975c35
İlker Kılıç <ilkrklc@gmail.com>
2020-11-22 10:48:03 +0300

    Add most simple version of getDivisors method

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
-import { exampleHelper } from '@helpers/helper';
+/* eslint-disable import/prefer-default-export */
 
-const exampleMethod = (options: ExampleInterface): number =>
-  options.n * exampleHelper();
+import { isWholeNumber } from '@helpers/number-integration.helpers';
+import { getAllDivisors } from '@helpers/divisor.helpers';
 
-export default exampleMethod;
+/**
+ * Get all divisors of the provided number
+ * @param {number} n number input
+ * @returns {number[]} Divisors of the provided number
+ */
+export const getDivisors = (n: number): number[] => {
+  if (isWholeNumber(n) === false)
+    throw new Error('Provided number must be a whole number.');
+
+  return getAllDivisors(n);
+};
